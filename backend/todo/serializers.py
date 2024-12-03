04044c6d9396c363e49d96805eb06d62b7c7d180
m-watanabe-8 <m-watanabe@eighteen.co.jp>
2024-12-03 14:24:44 +0900

    gitignotreを変更

--- backend/todo/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import TodoModel
-
-class TodoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TodoModel 
-        fields = ('id','title','content','status')
